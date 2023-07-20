57746a86f761877aa46067990ca60a169065a6b4
Pauline Muthoni <paugitonga6@gmail.com>
2023-07-20 06:25:45 +0300

    populated the footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,6 +2,33 @@ import React from 'react'
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div>
+        <hr/>
+        <div className='footer-container'>
+            <p>
+            © {new Date().getFullYear()} Pauline's Portfolio
+            </p>
+            <div className='social-icons'> 
+            <a
+            href="https://github.com/pauleen123"
+            aria-label="GitHub"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-github"></i>
+          </a>
+          <a
+            href="https://www.linkedin.com/in/pauline-gitonga-a9a0a1136/"
+            aria-label="LinkedIn"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <i className="fa-brands fa-linkedin"></i>
+          </a>
+
+            </div>
+
+        </div>
+    </div>
   )
 }

6501af5fe78849680862548ad1232c25d200ee3c
Pauline Muthoni <paugitonga6@gmail.com>
2023-07-20 14:35:20 +0300

    created and updated the contact page

---
+++ src/components/Contact.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+      <div className="contact-container">
+        <h2>Get In Touch</h2>
+        <p>If you want us to work together, have any questions or just want to speak with  me, my inbox is always open. Whether I'll try my best to get back to you! Cheers!</p>
+        <a href="mailto:paugitonga6@gmail.com" className='cta-btn'>Say Hello</a>
+      </div>
+    )
+  }
+  
+  export default Contact;

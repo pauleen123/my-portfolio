320472096f877a0a9538b7217594ef1b7cbe84e8
Pauline Muthoni <paugitonga6@gmail.com>
2023-07-20 06:13:06 +0300

    created and populated the navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+
+export default function Navbar() {
+  return (
+    <div className='nav-container'>
+     <div className='nav-logo'>
+        <Link href={'/'}>
+            Pauline's Portfolio
+        </Link>
+     </div>
+     <a href='' className='cta-btn'>Resume</a>
+    </div>
+  )
+}

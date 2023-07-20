fad7cb961020380547fecb062bf02189b83764a1
Pauline Muthoni <paugitonga6@gmail.com>
2023-07-20 08:22:53 +0300

    created the _app.js file and populated it

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-const Layout = () => {
+const Layout = ({children}) => {
   return (
-    <div>Layout</div>
+    <div>
+        <Navbar />
+        <main>{children}</main>
+        <Footer />
+    </div>
   )
 }
 
